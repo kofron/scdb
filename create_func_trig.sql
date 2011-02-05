829ecf978c59290eef2a312c86719c39c581f1c0
Jared Kofron <jared.kofron@gmail.com>
2011-02-04 22:50:11 -0800

    now works correctly UNLESS the insert is on stale data

--- create_func_trig.sql
+++ create_func_trig.sql
@@ -1,4 +1,10 @@
-create or replace function ensure_table_routing()
+-- master_table_routing (trigger)
+-- fires before inserts are made to meas_master.  takes the new row
+-- and instead of inserting the row into meas_master dynamically
+-- determines the tablename based on the timestamp and inserts the
+-- row there.  this operation executes in (near) constant time as the
+-- only operations are the string ops and an insert.
+create or replace function master_table_routing()
 	returns trigger as $$
 	declare
 		year integer = extract(year from NEW.ts)::integer;
@@ -24,6 +30,91 @@ create or replace function ensure_table_routing()
 	end
 $$ language plpgsql;
 
+-- daily_table_routing (trigger)
+-- fires when inserts are made to daily_master.  performs the same task
+-- as master_table_routing but returns NULL, as it is not a member of a
+-- chain of triggers but rather the last action to be performed.  it is
+-- only called during the updating of averages.
+create or replace function daily_table_routing()
+       returns trigger as $$
+       declare
+		year integer = extract(year from NEW.measdate)::integer;
+		tablename text = 'y' || year || 'avgDay';
+       begin	       
+       		execute 'insert into'
+			|| ' '
+			|| tablename
+			|| '(ucount, hostname, card, channel,'
+			|| 'measdate, minval, maxval, avgval) '
+			|| 'values ('
+			|| NEW.ucount
+			|| ','
+			|| quote_literal(NEW.hostname)
+			|| ','
+			|| quote_literal(NEW.card)
+			|| ','
+			|| NEW.channel
+			|| ','
+			|| quote_literal(NEW.measdate)
+			|| ','
+			|| NEW.minval
+			|| ','
+			|| NEW.maxval
+			|| ','
+			|| NEW.avgval
+			|| ')';
+		return NULL;
+       end
+$$ language plpgsql;
+
+-- hourly_table_routing (trigger)
+-- fires when inserts are made to hourly_master.  performs essentially
+-- the same task as master_table_routing but returns NULL, as it is not
+-- a member of a chain of triggers.  should only be called during a table
+-- 'flush' of the staging area.
+create or replace function hourly_table_routing()
+       returns trigger as $$
+       declare
+		year integer = extract(year from NEW.ts)::integer;
+		month integer = extract(month from NEW.ts)::integer;
+		tablename text = 'y' || year || 'm' || 'avgHour';
+       begin	       
+       		execute 'insert into'
+			|| tablename
+			|| 'values ('
+			|| NEW.*
+			|| ')';
+		return NULL;
+       end
+$$ language plpgsql;
+
+-- minute_table_routing (trigger)
+-- fires when inserts are made to minute_master.  performs essentially
+-- the same task as (hourly/master)_table_routing but returns NULL, as
+-- it is not a member of a chain of triggers.  should only be called 
+-- during a table 'flush' of the staging area.
+create or replace function minute_table_routing()
+       returns trigger as $$
+       declare
+		year integer = extract(year from NEW.ts)::integer;
+		month integer = extract(month from NEW.ts)::integer;
+		tablename text = 'y' || year || 'm' || 'avgMinute';
+       begin	       
+       		execute 'insert into'
+			|| tablename
+			|| 'values ('
+			|| NEW.*
+			|| ')';
+		return NULL;
+       end
+$$ language plpgsql;
+
+-- update_daily_avg (trigger)
+-- scans the daily staging table for the correct row and recalculates
+-- the rolling average for the day based on new values.  if the row does
+-- not exist in the daily staging table, it is created.  at the end of
+-- this trigger, a function is called which flushes old rows to the final
+-- (much larger) table and deletes those rows from the staging area.
 create or replace function update_daily_avg()
        returns trigger as $$
        declare
@@ -35,46 +126,27 @@ create or replace function update_daily_avg()
 		newmax real;
 		oldc int;
 		oldavg real;
-       		year integer = extract(year from NEW.ts)::integer;
-		tablename text = 'y' || year || 'avgDay';
        begin
 		-- Strategy:
 		-- Try to update a row.  If the row doesn't exist,
 		-- we create it instead.
-		select * into prow from daily_master dm where 
-		       (dm.day = newday 
+		select * into prow from daily_avg_stage stage where 
+		       (stage.measdate = newday 
 		       and
-		       dm.hostname = NEW.hostname
+		       stage.hostname = NEW.hostname
 		       and
-		       dm.card = NEW.card
+		       stage.card = NEW.card
 		       and 
-		       dm.channel = NEW.channel
-		       and
-		       extract(year from dm.day)::int = year);
+		       stage.channel = NEW.channel);
 		if not found then
-	  	   execute 'insert into '
-		   	|| tablename
-			|| '(ucount,hostname,card,channel,'
-			|| 'day,minval,maxval,avgval)'
-			|| ' '
-			|| 'values ('
-			|| 1
-			|| ','
-			|| quote_literal(NEW.hostname)
-			|| ','
-			|| quote_literal(NEW.card)
-			|| ','
-			|| NEW.channel
-			|| ','
-			|| quote_literal(newday)
-			|| ','
-			|| NEW.value
-			|| ','
-			|| NEW.value
-			|| ','
-			|| NEW.value
-			|| ')';
-			return NEW;
+		   insert into 
+		   	  daily_avg_stage
+				(ucount,hostname,card,
+			  	channel,measdate,minval,maxval,avgval) 
+			  values
+				(1,NEW.hostname,NEW.card,
+				NEW.channel,newday,
+				NEW.value,NEW.value,NEW.value);
 		else
 			oldc   := prow.ucount;
 			oldavg := prow.avgval;
@@ -87,7 +159,7 @@ create or replace function update_daily_avg()
 			elsif NEW.value > newmax then
 			   newmax := NEW.value;
 			end if;
-			update daily_master 
+			update daily_avg_stage 
 			set
 				ucount	  = newc,
 				avgval    = newavg,
@@ -95,105 +167,50 @@ create or replace function update_daily_avg()
 				maxval	  = newmax
 			where	  
 				row_id = prow.row_id;
-								
-			return NEW;
 		end if;
-		
+		perform flush_and_destroy_daily();	
+		return NULL;
        end		    
 $$ language plpgsql;
 
-create or replace function update_hourly_avg()
-       returns trigger as $$
+-- flush_and_destroy_daily (function)
+-- this is a function that scans the daily_avg_stage table for stale
+-- data (defined as older than 20 days), and flushes it to the long
+-- term (much larger) daily average tables.  rows successfully committed
+-- to the long term daily average tables are deleted from the staging 
+-- table.
+create or replace function flush_and_destroy_daily()
+       returns integer as $$
        declare
-		prow daily_master%ROWTYPE;
-		newday date = (NEW.ts)::date;
-		newc real;
-		newavg real;
-		newmin real;
-		newmax real;
-		oldc int;
-		oldavg real;
-       		yr integer = extract(year from NEW.ts)::integer;
-		mo integer = extract(month from NEW.ts)::integer;
-		nhr integer = extract(hour from NEW.ts)::integer;
-		tablename text = 'y' || yr || 'm' || mo || 'avgHour';
+	srow daily_master%rowtype;
        begin
-		-- Strategy:
-		-- Try to update a row.  If the row doesn't exist,
-		-- we create it instead.
-		select * into prow from hourly_master hm where 
-		       (hm.day = newday 
-		       and
-		       hm.hostname = NEW.hostname
-		       and
-		       hm.card = NEW.card
-		       and 
-		       hm.channel = NEW.channel
-		       and
-		       extract(year from hm.day)::int = yr
-		       and
-		       extract(month from hm.day)::int = mo
-		       and
-		       hm.hr = nhr);
-		if not found then
-	  	   execute 'insert into '
-		   	|| tablename
-			|| '(ucount,hostname,card,channel,'
-			|| 'day,hr,minval,maxval,avgval)'
-			|| ' '
-			|| 'values ('
-			|| 1
-			|| ','
-			|| quote_literal(NEW.hostname)
-			|| ','
-			|| quote_literal(NEW.card)
-			|| ','
-			|| NEW.channel
-			|| ','
-			|| quote_literal(newday)
-			|| ','
-			|| nhr
-			|| ','
-			|| NEW.value
-			|| ','
-			|| NEW.value
-			|| ','
-			|| NEW.value
-			|| ')';
-			return null;
-		else
-			oldc   := prow.ucount;
-			oldavg := prow.avgval;
-			newc   := prow.ucount + 1;
-			newavg := ((oldc*oldavg)+NEW.value)/newc;
-			newmin := prow.minval;
-			newmax := prow.maxval;
-			if NEW.value < newmin then
-			   newmin := NEW.value;
-			elsif NEW.value > newmax then
-			   newmax := NEW.value;
-			end if;
-			update hourly_master 
-			set
-				ucount	  = newc,
-				avgval    = newavg,
-				minval	  = newmin,
-				maxval	  = newmax
-			where	  
-				row_id = prow.row_id;
-			return null;
-		end if;		
-       end		    
+		for srow in 
+		    select * from daily_avg_stage s
+  		    where extract(day from age(s.measdate)) < -20
+		loop
+		    begin  
+		    	   delete from 
+					daily_avg_stage
+				  where
+					row_id = srow.row_id; 
+		    	   insert into daily_master values(srow.*);
+		    end;	   
+		end loop;
+		return 0;
+       end    
 $$ language plpgsql;
 
+--------------
+-- triggers --
+--------------
 create trigger master_routing
 	before insert on meas_master
-	for each row execute procedure ensure_table_routing();
+	for each row execute procedure master_table_routing();
 
 create trigger update_daily_averages
        before insert on meas_master
        for each row execute procedure update_daily_avg();
 
-create trigger update_hourly_averages
-       before insert on meas_master
-       for each row execute procedure update_hourly_avg();
+create trigger daily_routing
+       before insert on daily_master
+       for each row execute procedure daily_table_routing();
